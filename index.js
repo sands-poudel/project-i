754b14365839f42459dcdc23832de91b7b00f20e
Sandesh poudel <sandeshpoudel@192.168.1.8>
2025-07-26 12:49:20 +0545

    responsive

---
+++ index.js
@@ -0,0 +1,10 @@
+
+    function shownavside() {
+      const sidebar = document.querySelector('.navside')
+      sidebar.style.display = 'flex';
+
+    }
+    function hidenavside() {
+      const sidebar = document.querySelector('.navside')
+      sidebar.style.display = 'none';
+    }
